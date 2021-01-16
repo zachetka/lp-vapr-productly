b31ff02963ad57c6b8c74f1f492abe363b368104
Zachetka <axi83@mail.ru>
2021-01-16 14:14:36 +0300

    add adaptive for the section blog

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,24 @@ $(function() {
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        infinite: true,
+        speed: 300,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 540,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 });
